c0c6fadd9b52c697c8e52993790d8f261f855bc2
col3name <ya.mikushoff@gmail.com>
2022-10-24 19:36:14 +0400

    chore: update sql scripts

---
+++ data/migrations/postgres/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO customers(name)
+VALUES ('lenon'),
+       ('sara');
+INSERT INTO products (name)
+VALUES ('macbook air 13.3 m1 16gb 512gb'),
+       ('macbook pro 14.2 m1 pro 16gb 512'),
+       ('iphone 12 256gb');
+INSERT INTO orders (product_id, client_id)
+VALUES (1, 1),
+       (2, 1),
+       (3, 2);
