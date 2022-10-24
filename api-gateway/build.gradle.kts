dc777235b274e6392b1dad22929551536edfcccb
col3name <ya.mikushoff@gmail.com>
2022-10-24 18:14:41 +0400

    chore: cleanup build.gradle.kts on the api-gateway module

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -23,8 +23,6 @@ dependencies {
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
-//    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
-//    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 
     implementation("com.google.code.gson:gson:2.9.1")
